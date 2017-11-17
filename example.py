1a1f4343fd7d08ffe3e2c38b40fb8ee4725c7c56
Kerwin Jorbina <kerwin.jorbina@gmail.com>
2017-11-16 20:15:50 -0500

    changing encoded event names as numbers to its original string - allowing encoding for the entire log

--- example.py
+++ example.py
@@ -15,7 +15,6 @@ def frequency_encode(data):
 def simple_index_encode(data, prefix):
 	import simple_index_encoder
 	encoding_method = simple_index_encoder.SimpleIndexEncoder()
-	print data
 	encoded_trace = encoding_method.encode_trace(data, prefix)
 	return encoded_trace
 
@@ -50,9 +49,8 @@ encoder.write_df_to_csv(encoder.df, "xes_to_csv_"+filename+".csv")
 # encoded_trace = frequency_encode(encoder.df)
 # encoder.write_df_to_csv(encoded_trace, "frequency_encode_"+filename+".csv")
 
-# encoded_trace = simple_index_encode(encoder.df, 5)
-# print encoded_trace
-# encoder.write_df_to_csv(encoded_trace, "simple_index_encode_"+filename+".csv")
+encoded_trace = simple_index_encode(encoder.df, 1)
+encoder.write_df_to_csv(encoded_trace, "simple_index_encode_"+filename+".csv")
 
 # encoded_trace = index_latest_payload_encode(encoder.df, encoder.event_attributes, 2)
 # encoder.write_df_to_csv(encoded_trace, "index_latest_payload_encode_"+filename+".csv")
@@ -61,9 +59,9 @@ encoder.write_df_to_csv(encoder.df, "xes_to_csv_"+filename+".csv")
 # encoder.write_df_to_csv(encoded_trace, "complex_encode_"+filename+".csv")
 
 # level 0, 1, 2 and 3 only
-level = 2
-encoded_trace = intercase_encode(encoder.df, filename, level=level)
-train, test = encoder.split_data(encoded_trace)
-encoder.write_df_to_csv(encoded_trace, "intercase_encode_"+filename+"_level"+str(level)+".csv")
-encoder.write_df_to_csv(train, "intercase_encode_train"+filename+"_level"+str(level)+".csv")
-encoder.write_df_to_csv(test, "intercase_encode_test"+filename+"_level"+str(level)+".csv")
+# level = 2
+# encoded_trace = intercase_encode(encoder.df, filename, level=level)
+# train, test = encoder.split_data(encoded_trace)
+# encoder.write_df_to_csv(encoded_trace, "intercase_encode_"+filename+"_level"+str(level)+".csv")
+# encoder.write_df_to_csv(train, "intercase_encode_train"+filename+"_level"+str(level)+".csv")
+# encoder.write_df_to_csv(test, "intercase_encode_test"+filename+"_level"+str(level)+".csv")
--- simple_index_encoder.py
+++ simple_index_encoder.py
@@ -16,6 +16,7 @@ class SimpleIndexEncoder:
 	def set_log(self, log):
 		self.log = log
 
+	# if prefix length is equal to 0, it encodes the entire log at every prefix
 	def encode_trace(self, data, prefix_length=1):
 		data_encoder = encoder.Encoder()
 		events = data_encoder.get_events(data).tolist()
@@ -26,6 +27,12 @@ class SimpleIndexEncoder:
 		columns.append("event_nr")
 		columns.append("remaining_time")
 		columns.append("elapsed_time")
+
+		init_prefix = prefix_length
+		if prefix_length == 0:
+			init_prefix = 1
+			prefix_length = max(data['event_nr'])
+
 		for i in range(1, prefix_length+1):
 			columns.append("prefix_"+str(i))
 
@@ -33,19 +40,24 @@ class SimpleIndexEncoder:
 
 		for case in cases:
 			df = data[data['case_id'] == case]
-			event_length = prefix_length
-			case_data = []
-			case_data.append(case)
-			case_data.append(event_length)
-			remaining_time = data_encoder.calculate_remaining_time(df, event_length)
-			case_data.append(remaining_time)
-			elapsed_time = data_encoder.calculate_elapsed_time(df, event_length)
-			case_data.append(elapsed_time)
 
-			case_events = df[df['event_nr'] <= event_length]['activity_name'].tolist()
-			for e in case_events:
-				case_data.append(events.index(e)+1)
-			encoded_data.append(case_data)
+			for event_length in range(init_prefix, prefix_length+1):
+				if len(df) < event_length:
+					continue
+
+				case_data = []
+				case_data.append(case)
+				case_data.append(event_length)
+				remaining_time = data_encoder.calculate_remaining_time(df, event_length)
+				case_data.append(remaining_time)
+				elapsed_time = data_encoder.calculate_elapsed_time(df, event_length)
+				case_data.append(elapsed_time)
+
+				case_events = df[df['event_nr'] <= event_length]['activity_name'].tolist()
+				for e in case_events:
+					#case_data.append(events.index(e)+1) #numerical representation
+					case_data.append(e) #string representation
+				encoded_data.append(case_data)
 
 		df = pd.DataFrame(columns=columns, data=encoded_data)
 		return df
